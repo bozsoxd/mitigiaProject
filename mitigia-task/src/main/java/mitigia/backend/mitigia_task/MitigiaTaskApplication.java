e9d1bb9e7f0ee87ce7254fab1272b5481355f504
Nagy Bozsó <bozso@Nagy-MacBook-Pro.local>
2024-11-01 11:50:42 +0100

    Save before jar

--- mitigia-task/src/main/java/mitigia/backend/mitigia_task/MitigiaTaskApplication.java
+++ mitigia-task/src/main/java/mitigia/backend/mitigia_task/MitigiaTaskApplication.java
@@ -1,9 +1,13 @@
 package mitigia.backend.mitigia_task;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class MitigiaTaskApplication {
 
 	public static void main(String[] args) {
